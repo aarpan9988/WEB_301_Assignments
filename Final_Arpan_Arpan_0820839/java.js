f244a54e23d4db4a70efb8da2184915750b9d224
aarpan9988 <122822502+aarpan9988@users.noreply.github.com>
2023-12-12 16:53:22 -0500

    Add files via upload

---
+++ Final_Arpan_Arpan_0820839/java.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $("#personal-form").validate({
+        rules: {
+            name: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 6
+            },
+            phone: {
+                required: true,
+                pattern: /^\d{3}-\d{3}-\d{4}$/
+            },
+            address:"required"
+        },
+        messages: {
+            name: "Please enter your name",
+            email: {
+                required: "Please enter your email",
+                email: "Please enter a valid email address"
+            },
+            password: {
+                required: "Please enter a password",
+                minlength: "Password must be at least 6 characters long"
+            },
+            phone: {
+                required: "Please enter a phone number",
+                pattern: "Please enter a valid US phone number (e.g., 123-456-7890)"
+            },
+            address:"Please enter your address"
+        }
+    });
+});
